fedd6f4508913479a8f0315ed802c136b452e6e2
Michał Potoczny <66637616+yougonnagetyours@users.noreply.github.com>
2021-08-31 09:53:53 +0000

    New version of code

--- script.js
+++ script.js
@@ -1,49 +1,10 @@
-const numbers=[1,3,5,2,6,8,9];
-//let newNumbers=[];
-
-const addNumber = (array) => {
-  for (let i = 0; i<array.length ; i++) {
-    array[i] += 1;
-  }
-}
-
-const showAbove5 = (array) => {
-  let newArray = [];
-
-  for (let i = 0; i<array.length; i++){
-    if (array[i] >= 5){
-      newArray = [...newArray, array[i]]
-    }
-  }
-}
-
-//For Each
-const forEachFn = (array, callback) => {
-
-  callback(array);
-
-  return array;
-};
-
-//MAP
-const mapFn = (array, callback) => {
+function forEachFn(array, callback){
+  if (!Array.isArray(array)) throw new Error("wprowadź tablicę");
+  if (callback != function) throw new Error("to nie jest funkcja");
 
   const newArray = [...array];
 
-  callback(newArray);
-
-  return newArray;
-};
-
-//FILTER
-const filterFn = (array, callback) => {
-  callback(array);
-};
-
-//OUTPUT
-
-//console.log(numbers);
-//console.log(forEachFn(numbers,addNumber));
-//console.log(mapFn(numbers,addNumber));
-console.log(filterFn(numbers,showAbove5));
-//console.log(numbers);
+  for (const index in newArray) {
+    const id = parseInt
+  }
+}
